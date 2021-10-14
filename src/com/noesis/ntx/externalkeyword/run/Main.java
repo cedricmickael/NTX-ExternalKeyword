846e6716d87dc051946123ffdcd03457995d44b1
paulo-pimenta <91731703+paulo-pimenta@users.noreply.github.com>
2021-10-14 17:53:50 +0100

    Update Main.java

--- src/com/noesis/ntx/externalkeyword/run/Main.java
+++ src/com/noesis/ntx/externalkeyword/run/Main.java
@@ -12,7 +12,7 @@ import org.openqa.selenium.support.ui.ExpectedConditions;
 import org.openqa.selenium.support.ui.WebDriverWait;
 
 public class Main {
-
+//alteração de codigo
 	/**
 	 * @param args
 	 */
